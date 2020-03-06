485b5d63336c837564c4f68a1e46afa2da626ef6
andrelai1981 <jfreak.sg@gmail.com>
2020-03-06 00:39:05 -0600

    form validation for new user page

--- MedicalRecords-SPA/src/app/user/user-new/user-new.component.ts
+++ MedicalRecords-SPA/src/app/user/user-new/user-new.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { Router } from '@angular/router';
 import { AlertifyService } from 'src/app/_services/alertify.service';
-import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-new',
@@ -11,23 +11,42 @@ import { UserService } from 'src/app/_services/user.service';
   styleUrls: ['./user-new.component.css']
 })
 export class UserNewComponent implements OnInit {
-  user: any = {};
+  user: User;
+  registerForm: FormGroup;
   constructor(private router: Router, private userService: UserService
-    , private alertify: AlertifyService) { }
+    , private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required],
+    }, {validator: this.passwordMatchValidator});
   }
 
   addUser() {
-    console.log(this.user);
-    this.userService.create(this.user).subscribe(() => {
-      this.alertify.success('Added user successfully');
-      this.router.navigate(['/users']);
-    }, error => {
-      this.alertify.error(error);
-    }, () => {
-      this.router.navigate(['/users']);
-    });
+    debugger;
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.userService.create(this.user).subscribe(() => {
+        this.alertify.success('Added user successfully');
+        this.router.navigate(['/users']);
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/users']);
+      });
+    }
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   cancel() {
